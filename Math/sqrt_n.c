24d80526fc4f9d89939cebd4103c1f6952c524fe
yinhao <yinhao@cnic.cn>
2019-06-19 19:54:57 +0800

    test for sqrt_n

--- Math/sqrt_n.c
+++ Math/sqrt_n.c
@@ -3,7 +3,7 @@
 *  Author      : H.Yin
 *  Email       : csustyinhao@gmail.com
 *  Created     : 2019-06-17 17:06:49(+0800)
-*  Modified    : 2019-06-18 15:58:18(+0800)
+*  Modified    : 2019-06-19 19:54:24(+0800)
 *  GitHub      : https://github.com/H-Yin/data_structure_in_action.git
 *  Description : implement sqrt(n) without math.h
 *******************************************************************/
@@ -11,14 +11,14 @@
 #include<stdio.h>
 #include<math.h>
 
-#define abs(x) ((x)>0?(x):-(x))
-
 double sqrt_bisection(double n){
-    double low = 0, high= 1 + n / 2.0, mid, x;
+    double low = 0, high= (1 + n) / 2.0, mid, x;
+    int i = 0;
     if(n < 0){
         return -1;
     }
-    while(high - low >= 1e-10){
+    /*TODO : fix bug when 1e-10   */
+    while(high - low >= 1e-6){
         mid = (low + high) / 2.0;
         x = mid * mid;
         if(x == n){
@@ -28,29 +28,39 @@ double sqrt_bisection(double n){
         }else{
             high = mid;
         }
+        i++;
     }
     mid = (low + high) / 2.0;
     return mid;
 }
 
 double sqrt_newton(double n){
-    double xj = 0, xi = 1 + n / 2.0;
+    int i=0;
+    double xj = 0, xi = (1 + n) / 2.0;
     if(n < 0) return -1;
     if(n == 0) return 0;
     while(1){
         xj = (xi + n / xi) / 2.0;
-        if(abs(xj - xi) < 1e-10){
+        if(xi - xj < 1e-6){
             break;
         }
         xi = xj;
+        i++;
     }
     return xj;
 }
 
 int main(){
-    int i =0;
-    for(i=0; i<=100; i++){
-        printf("%.10f\n", sqrt_newton(i));
+    double i = 31e10, a, b, c;
+    for(i; i<=31e10; i+=1){
+        /* a = sqrt(i); */
+        b = sqrt_bisection(i);
+        printf("%.10lf\n", b);
+        /* c = sqrt_newton(i); */
+        /* if(a != b) */
+            /* printf("1:%.10f\t%.10lf\n", a, b); */
+        /* if(a != c) */
+            /* printf("2:%.10f\t%.10lf\n", a, c); */
     }
     return 0;
 }
